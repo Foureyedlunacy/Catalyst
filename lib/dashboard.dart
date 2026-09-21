226f3243940f59d1b7965e2a9daec974eab518e3
Foureyedlunacy <gyanshu300@gmail.com>
2026-09-22 02:57:57 +0530

    Fix import path for hnavbar in dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,5 @@
-import 'package:catalyst/homecomponents/h_navbar.dart';
-import 'package:catalyst/Homecomponents/navbarlink.dart';
+import 'package:catalyst/homecomponents/hnavbar.dart';
+import 'package:catalyst/homecomponents/navbarlink.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
