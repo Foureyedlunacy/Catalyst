f90bcd296a186c9748896e6b8771fb7805ccd55f
Foureyedlunacy <gyanshu300@gmail.com>
2026-09-22 03:04:17 +0530

    Update panelcol.dart

--- lib/homecomponents/panelcol.dart
+++ lib/homecomponents/panelcol.dart
@@ -1,4 +1,4 @@
-import 'package:catalyst/homecomponents/components/projecttitlecard.dart';
+import 'package:Catalyst/homecomponents/components/projecttitlecard.dart';
 import 'package:flutter/material.dart';
 
 class Panelcol extends StatelessWidget {
