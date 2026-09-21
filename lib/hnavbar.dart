f7ca2226b37fbbea80283cdd9ff908a0cae802f2
Foureyedlunacy <gyanshu300@gmail.com>
2026-09-22 02:55:49 +0530

    Update import path for navbarlink.dart

--- lib/hnavbar.dart
+++ lib/hnavbar.dart
@@ -1,5 +1,5 @@
 
-import 'package:catalyst/Homecomponents/smaller%20component/navbarlink.dart';
+import 'package:catalyst/Homecomponents/navbarlink.dart';
 import 'package:flutter/material.dart';
 //0xFFf7e4cc  champagne/skin
 //0xFF325453  darkslate gray/dark blue
