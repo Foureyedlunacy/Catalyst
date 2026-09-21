f2d6e67d466a0aed4ff20bece17ad3e9065f31c0
Foureyedlunacy <gyanshu300@gmail.com>
2026-09-22 03:02:16 +0530

    Fix import case sensitivity for homecomponents

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,5 @@
-import 'package:catalyst/homecomponents/hnavbar.dart';
-import 'package:catalyst/homecomponents/navbarlink.dart';
+import 'package:Catalyst/homecomponents/hnavbar.dart';
+import 'package:Catalyst/homecomponents/navbarlink.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
