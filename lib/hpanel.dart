d7bb03f492df1e7ddd32ea592b8fd04635721ecb
Foureyedlunacy <gyanshu300@gmail.com>
2026-09-22 03:03:29 +0530

    Fix import case sensitivity for panelcol.dart

--- lib/hpanel.dart
+++ lib/hpanel.dart
@@ -1,4 +1,4 @@
-import 'package:catalyst/homecomponents/panelcol.dart';
+import 'package:Catalyst/homecomponents/panelcol.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
