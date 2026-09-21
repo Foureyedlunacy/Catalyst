2559aa0d31cfa3fbe16d1dc80906911ab7f88412
Foureyedlunacy <gyanshu300@gmail.com>
2026-09-22 02:57:38 +0530

    Fix import path for hnavbar.dart

--- lib/github.dart
+++ lib/github.dart
@@ -1,5 +1,5 @@
 
-import 'package:catalyst/homecomponents/h_navbar.dart';
+import 'package:catalyst/homecomponents/hnavbar.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flutter/material.dart';
